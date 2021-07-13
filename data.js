26e65b9ee7ca036ea842cecef621a1bfc5327aaa
Nitheshkumar <iamnk1433@gmail.com>
2021-07-13 10:28:02 +0000

    Initial commit.

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+    cost: 10,
+    name: 'buildHouse',
+    tasks: [
+        {
+            cost: 5,
+            name: 'purchase material',
+            tasks: [
+                {
+                    cost: 10,
+                    name: 'purchase cement',
+                },
+                {
+                    cost: 15,
+                    name: 'purchase steel',
+                }
+            ]
+        },
+        {
+            name: 'invite people',
+        }
+    ],
+}
